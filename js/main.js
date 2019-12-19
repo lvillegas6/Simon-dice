610593d71a3db613fad97c39c403c2deff3eb906
Luis Andres Villegas <theeniig@gmail.com>
2019-12-19 10:27:43 -0500

    Generando una secuencia de numeros.
    Utilizando "new Array()"

--- js/main.js
+++ js/main.js
@@ -7,17 +7,33 @@ const boton = document.getElementById('boton');
 class Juego{
 
     constructor(){
-        this.inicializar();
+        this.inicializar(); //este método siempre va dentro del constructor.
+        this.generarSecuencia();
     }
 
     inicializar(){
         boton.classList.add('hide');
+        this.nivel = 1;
+        this.colores = {
+            //celeste: celeste, esto es equivalente.
+            celeste,
+            violeta,
+            naranja,
+            verde
+        }
     }
+
+    generarSecuencia(){
+                        //generar array de 10 elemeto, luego colocando el valor 0 a cada elemento
+        this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4));
+    }
+
 }
 
 
 function empezarJuego(e){
-    var juego = new Juego();
+    window.juego = new Juego(); //Poner esta variable dentro de windows, para poder debugearla. 
+    //var juego = new Juego();
 }
 
 boton.addEventListener('click', empezarJuego);
